a78d46e587995b9654715e007a99507de6810adb
Anderson Bravalheri <andersonbravalheri@gmail.com>
2023-04-26 15:29:57 +0100

    Add debug statements for visibility

--- setuptools/version.py
+++ setuptools/version.py
@@ -1,6 +1,6 @@
 from ._importlib import metadata
 
 try:
-    __version__ = metadata.version('setuptools')
+    __version__ = metadata.version('setuptools') or 'unknown'
 except Exception:
     __version__ = 'unknown'
